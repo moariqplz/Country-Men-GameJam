cb93279d45df9598ddf6f854e2f4d231cf954c47
Caleb Smith <calebsmth54@gmail.com>
2016-09-24 00:18:13 -0800

    Added script for detecting player collision with enemies

---
+++ Country_Men_Game_Clubb/Assets/Scripts/PlayerScripts/PlayerCollideAI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollideAI : MonoBehaviour {
+
+	public GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+
+		if(gameController != null) {
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+
+		else {
+			Debug.Log("PlayerCollideAI could not find 'GameController'!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+	
+		if(other.tag == "Enemy1" || other.tag == "Enemy2")
+		{
+			Debug.Log("Player was touched by Enemy1 or Enemy2!");
+		}
+		else if(other.tag == "Enemy3") {
+			Debug.Log("Player was touched by Enemy3, do nothing!");
+		}
+	}
+}
