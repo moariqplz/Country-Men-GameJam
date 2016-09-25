bfe522e85e21df7e53932dd676d718641d08a6f7
Caleb Smith <calebsmth54@gmail.com>
2016-09-24 19:14:35 -0800

    Added the monster mixers into unity, created a micinput prefab (add to player as a child object)

---
+++ Country_Men_Game_Clubb/Assets/Audio Mixers/README.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class README : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	//Little Enemy is lured by "LittleGirl" and scared away by "DemonLady"
+    //Beserker is Lured by "Basic"
+    //FatEnemy is lured by "FatGuy"
+    //NOTE**** In the event the player calls to an enemy using the wrong/unlisted vocie effect, will 
+    //resulte in the enemy entering an AGRO STATE. Agro is when the enemy comes towards the player and tries to kill you
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
