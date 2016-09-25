f5ef33733ff4e94ec91d8bf611bc6a174e6dec88
Caleb Smith <calebsmth54@gmail.com>
2016-09-25 10:49:33 -0800

    Added looping audio and model to generator

--- Country_Men_Game_Clubb/Assets/Scripts/Generator.cs
+++ Country_Men_Game_Clubb/Assets/Scripts/Generator.cs
@@ -3,19 +3,66 @@ using System.Collections;
 
 public class Generator : MonoBehaviour {
 	public bool isActive;
+	public bool isRunning;
+	public bool isStarting;
+
+	public AudioClip generatorStartSound;
+	public AudioClip generatorLoopSound;
+
+	private AudioSource myAudio;
+
 	//public GameObject door;
 	// Use this for initialization
 	void Start () {
-		isActive = false;
+		isActive = true;
+		isStarting = false;
+		isRunning = false;
+
+		myAudio = GetComponent<AudioSource>();
+		myAudio.clip = generatorStartSound;
 	}
+
+	void Update() {
+
+		if( isRunning && isStarting ) {
+			
+			// Are we finished starting?
+			if( !myAudio.isPlaying ) {
+
+				//Start looping
+				myAudio.clip = generatorLoopSound;
+				myAudio.loop = true;
+				myAudio.Play();
+
+				isStarting = false;
+			}
+		}
+	}
+
 	void OnTriggerStay(Collider other)
 	{
-		Debug.Log (other.ToString ());
-		if (other.tag == "Player"  && Input.GetKeyDown(KeyCode.E))
+		Debug.Log (other.tag);
+		if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E) )
 		{
 			Debug.Log ("test");
-			isActive = true;
-			//door.GetComponent<DoorLightSwitcher> ().checkGenerator ();
+
+			// Don't activate if we're already active
+			if( isActive && !isRunning ) {
+				Debug.Log ("test");
+				isRunning = true;
+				isStarting = true;
+				myAudio.Play();
+				//door.GetComponent<DoorLightSwitcher> ().checkGenerator ();
+			}
 		}
+	}
+		
+	void Deactivate() {
+
+		isActive = false;
+		isRunning = false;
+		isStarting = false;
+
+		myAudio.Stop();
 	}
 }
