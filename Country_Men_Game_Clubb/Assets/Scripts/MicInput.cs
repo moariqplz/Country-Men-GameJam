79a7b3dacfeedba13b5b573c5776811d3e13801e
Caleb Smith <calebsmth54@gmail.com>
2016-09-24 21:42:10 -0800

    Updated push-to-talk on

--- Country_Men_Game_Clubb/Assets/Scripts/MicInput.cs
+++ Country_Men_Game_Clubb/Assets/Scripts/MicInput.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Audio;
 
 [RequireComponent(typeof(AudioSource))]
 public class MicInput : MonoBehaviour {
@@ -8,17 +9,20 @@ public class MicInput : MonoBehaviour {
 	public float sensitivity = 100;
 	public float loudness = 0;
 
+	public AudioMixerGroup mixer0;
+	public AudioMixerGroup mixer1;
+	public AudioMixerGroup mixer2;
+	public AudioMixerGroup mixer3;
+
 	// Private variables for playing sound, the soundclip itself and the currently used audio device
 	private AudioSource audioSource; // Where we play our sample from
 	private AudioClip audioSample; // Sample of what the player just said
 	private string audioDevice = null; // The device we're using
 	private int maxFreq;
 
-	private float getDeviceAfterDelay;
-
-	// For room level detection
-	private bool roomLevelCollected = false;
-	private float roomLevel;
+	public float audioActiveTime = 3.5f;
+	private float audioActiveEndTime;
+	private bool audioActive = false;
 
 	// Start of play
 	void Start () {
@@ -40,21 +44,28 @@ public class MicInput : MonoBehaviour {
 		loudness = GetAverageVolume()*sensitivity;
 
 		// Start recording
-		if( Input.GetKeyDown(KeyCode.R) ) {
-			audioSource.clip = Microphone.Start(audioDevice, true, 256, maxFreq);
-			// Collect room tone once
-			//StartCoroutine(CollectLevels());
-			Debug.Log("keydown!");
+		if( Input.GetKeyDown(KeyCode.Alpha1) ) {
+			SetMixer(1);
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha0) ) {
+			SetMixer(0);
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha2) ) {
+			SetMixer(2);
+		}
+		if( Input.GetKeyDown(KeyCode.Alpha3) ) {
+			SetMixer(3);
 		}
 
-		else if( Input.GetKeyUp(KeyCode.R) ) {
-			//Microphone.End(audioDevice);
+		if( Input.GetKeyDown(KeyCode.Space) ) {
+			StartRecording();
 		}
 
-		if(Input.GetKeyDown(KeyCode.P)) {
-			audioSource.Play();
+		if( audioActive && audioActiveEndTime <= Time.time )
+		{
+			StopRecording();	
 		}
-	
+
 	}
 
 	// Capture a microphone device from this list, return true if the first one we run into works
@@ -71,10 +82,28 @@ public class MicInput : MonoBehaviour {
 
 		// Get maxfrequency
 		int minFreq = 0;
-		Microphone.GetDeviceCaps(null, out minFreq, out maxFreq);
+		Microphone.GetDeviceCaps(audioDevice, out minFreq, out maxFreq);
 		if( minFreq == 0 && maxFreq == 0 ) {
 			maxFreq = 44100;
 		}
+	}
+
+	void StartRecording() {
+		// Start recording
+		audioSource.clip = Microphone.Start(audioDevice, true, 20, maxFreq);
+		while (!(Microphone.GetPosition(null) > 0)){}
+
+		audioSource.loop = false;
+		audioSource.Play();
+
+		audioActiveEndTime = Time.time + audioActiveTime;
+		audioActive = true;
+	}
+
+	void StopRecording() {
+		audioSource.Stop();
+		Microphone.End(audioDevice);
+		audioActive = false;
 	}
 
 	// Check the loudness of input
@@ -92,30 +121,28 @@ public class MicInput : MonoBehaviour {
 		return a/256;
 	}
 
-	IEnumerator CollectLevels() {
-		
-		// Start a test recording
-		audioSource.clip = Microphone.Start(audioDevice, true, 20, maxFreq);
+	public void SetMixer(int n) {
 
-		// Wait five seconds to collect room tone
-		yield return new WaitForSecondsRealtime(5.0f);
+		switch( n ) {
+		case 0:
+			audioSource.outputAudioMixerGroup = mixer0;	
+			break;
 
-		Microphone.End(audioDevice); // Stop recording
+		case 1:
+			audioSource.outputAudioMixerGroup = mixer1;
+			break;
 
-		//audioSource.mute = true;
-		audioSource.Play();
+		case 2:
+			audioSource.outputAudioMixerGroup = mixer2;
+			break;
 
-		int numSamples = 0;
+		case 3:
+			audioSource.outputAudioMixerGroup = mixer3;
+			break;
 
-		//Wait for the clip to finish playing
-		while( audioSource.isPlaying ) {
-			roomLevel += Mathf.Abs(loudness);
-			numSamples++;
-
-			yield return new WaitForSeconds(0.01f);
+		default:
+			audioSource.outputAudioMixerGroup = mixer0;
+			break;
 		}
-
-		roomLevel = roomLevel/numSamples;
-		Debug.Log("Room Level: " + roomLevel);
 	}
 }
